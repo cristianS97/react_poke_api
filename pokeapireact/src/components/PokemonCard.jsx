2a94cce8a3b5311f7413db4f096c5280301fcc3d
Cristian Sáez Mardones <cristiansaez97@gmail.com>
2023-01-12 01:46:31 -0300

    Se corrige detalle en la separación de los tipos pokemon

--- pokeapireact/src/components/PokemonCard.jsx
+++ pokeapireact/src/components/PokemonCard.jsx
@@ -25,7 +25,7 @@ export const PokemonCard = ({pokemon}) => {
         <img src={pokemonFetch.images[imageId]} className="card-img-top" alt={pokemonFetch.name}/>
         <div className="card-body">
           <h5 className="card-title">({pokemonFetch.id}) {pokemonFetch.name}</h5>
-          <p className="card-text">Types: {pokemonFetch.types.map(type => type + ', ')}</p>
+          <p className="card-text">Types: {pokemonFetch.types.join(', ')}</p>
           <button className="btn btn-primary" onClick={cambiarImagen}>{imageId === 0 ? 'Shiny' : 'Normal'}</button>
         </div>
       </div>
